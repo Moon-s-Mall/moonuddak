0d2e3ef472524b05a3d098607dd6868fc53fdc76
Jaewoo Kim <wodndhksk@gmail.com>
2022-11-27 20:32:51 +0900

    first commit

---
+++ src/main/java/com/moonuddak/MoonuddakApplication.java
@@ -0,0 +1,13 @@
+package com.moonuddak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonuddakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonuddakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moonuddak/MoonuddakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonuddak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonuddakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
